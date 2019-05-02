0b60f7780234976678709cd4ae2050c8ced02424
ifforget <1277186805@qq.com>
2019-05-03 00:09:19 +0800

    new

--- js/indexjq.js
+++ js/indexjq.js
@@ -1,21 +1,37 @@
 $(function(){
-
+    var number = ''
     $(".flow_bottom_btn").on("click",function(){
-        $(".flow_bottom_btn").css('dispaly','none')
-        // this.style.display = "none"
-       console.log("aaa")
+        $(".flow_bottom_btn").hide()
     })
 })
 
+$(".flow_bottom_top").find("li").on("click",function(){
+    $(".flow_bottom_btn").show()
+     number = $(this).index()+1
+    $(".flow_bottom_btnn img").attr("src","images/bg"+number+".jpg")
+  
+   
+})
 
-$(".lcleft").on("click",function(e){
-    $(".flow_bottom_btnn img").attr("src","images/bg4.jpg")
-   console.log("left")
+$(".lcleft").on("click",function(){
+    number++
+    if(number == 5 ){
+        number = 4
+        $(".flow_bottom_btnn img").attr("src","images/bg"+number+".jpg")
+    }else{
+        $(".flow_bottom_btnn img").attr("src","images/bg"+number+".jpg")
+    }
    return false;
 })
-$(".lcright").on("click",function(e){
-   
-    console.log("right")
+$(".lcright").on("click",function(){
+   number--
+
+   if(number == 0){
+      number = 1
+      $(".flow_bottom_btnn img").attr("src","images/bg"+number+".jpg")
+   }else{
+    $(".flow_bottom_btnn img").attr("src","images/bg"+number+".jpg")
+   }
     return false;
  })
     
